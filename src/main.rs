8fa34d1e9bba24129eda5c6d89b361c0a95a0cc3
Yusuke Hosonuma <tobi462@gmail.com>
2018-06-17 12:11:46 +0900

    refactor: use path

--- src/main.rs
+++ src/main.rs
@@ -4,49 +4,47 @@ use std::process::Command;
 use std::fs;
 use std::io::{BufWriter, Write};
 use clap::{Arg, App};
+use std::path::Path;
 
 fn main() {
     let matches = App::new("nene")
                           .version("0.1.0")
                           .author("tobi462 <tobi462@gmail.com>")
                           .about("Remove ANSI escape codes in file.")
-                          .arg(Arg::with_name("filename")
-                                .help("input filename")
+                          .arg(Arg::with_name("path")
+                                .help("input filepath")
                                 .required(true))
                           .arg(Arg::with_name("output")
-                                .help("output filename")
+                                .help("output filepath")
                                 .short("o")
                                 .long("out")
                                 .takes_value(true))
                           .get_matches();
 
-    // TODO: support path
-
-    let filename = matches.value_of("filename").unwrap();
-
-    let text = read_as_plaintext(&filename);
+    let path = Path::new(matches.value_of("path").unwrap());
+    let text = read_as_plaintext(&path);
 
     match matches.value_of("output") {
         None => println!("{}", text),
-        Some(filename) => write_newfile(&filename, &text),
+        Some(path) => write_newfile(&Path::new(path), &text),
     }
 }
 
-fn read_as_plaintext(filename: &str) -> String {
+fn read_as_plaintext(path: &Path) -> String {
 
     // remove ANSI escape code
     let output = Command::new("sed")
         .arg("-E")
         .arg("s/\\[([0-9]{1,2}(;[0-9]{1,2})*)?m//g") // Note: include esc char \e
-        .arg(filename)
+        .arg(path)
         .output()
         .unwrap();
 
     String::from_utf8(output.stdout).unwrap()
 }
 
-fn write_newfile(filename: &str, text: &str) {
-    let mut w = BufWriter::new(fs::File::create(filename.clone()).unwrap());
+fn write_newfile(path: &Path, text: &str) {
+    let mut w = BufWriter::new(fs::File::create(path.clone()).unwrap());
     w.write(text.as_bytes()).unwrap();
-    eprintln!("output: {}", filename);
+    eprintln!("output: {}", path.display());
 }
